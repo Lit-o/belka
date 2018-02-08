8bda11a099599e13a464580f7f85adbe9f5f36b2
Litvinenko Boris <boris12lit@gmail.com>
2018-02-08 16:40:10 +0300

    достилизовал и ускорил выпадение мобильного меню

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ $( document ).ready(function() {
   $( ".header-cross" ).hide();
   $( ".kebab-menu" ).hide();
   $( ".header-kebab" ).click(function() {
-    $( ".kebab-menu" ).slideToggle( "slow", function() {
+    $( ".kebab-menu" ).slideToggle( "middle", function() {
       $( ".header-kebab" ).hide();
       $( ".header-cross" ).show();
     });
   });
 
   $( ".header-cross" ).click(function() {
-    $( ".kebab-menu" ).slideToggle( "slow", function() {
+    $( ".kebab-menu" ).slideToggle( "middle", function() {
       $( ".header-cross" ).hide();
       $( ".header-kebab" ).show();
     });
