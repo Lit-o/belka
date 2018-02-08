5a4817683e1dc20c17c29b5f03242ebc59fb1859
Litvinenko Boris <boris12lit@gmail.com>
2018-02-08 20:58:34 +0300

    Стилизовал второй блок под таблетку со скролом. + Slick.min.js, подключил, настроил, работает!

--- js/script.js
+++ js/script.js
@@ -15,5 +15,42 @@ $( document ).ready(function() {
       $( ".header-kebab" ).show();
     });
   });
+  var timer;
+  $(window).on('resize', function(){
+    if ( timer ) clearTimeout(timer);
+    timer = setTimeout(function(){
+      togglePromotionsSlider();
+    }, 100);
+  });
+
+  togglePromotionsSlider();
+
+  function togglePromotionsSlider (){
+    $('#slider').slick({
+      mobileFirst: true,
+      infinite: true,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 0,
+          settings: {
+            slidesToShow: 1,
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 3,
+          }
+        },
+        {
+          breakpoint: 1279,
+          settings: 'unslick'
+        },
+      ]
+    });
+  }
 
 });
+
+
